2392422829c72201f4f39b30e3f0a6a34e88c335
Beren Erchamion <beren@houseofbeor.net>
2021-02-27 14:33:13 -0500

    fine tuning state handling

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -5,52 +5,54 @@ import '../providers/product_provider.dart';
 class ProductItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final product = Provider.of<Product>(context);
-    return ClipRRect(
-      borderRadius: BorderRadius.circular(5),
-      child: GridTile(
-        child: Container(
-          decoration: BoxDecoration(
-            boxShadow: [
-              BoxShadow(
-                color: Colors.black87,
-                blurRadius: 10.0,
-                spreadRadius: 7.0,
-              ),
-            ],
-          ),
-          child: GestureDetector(
-              onTap: () {
-                Navigator.of(context)
-                    .pushNamed('/product-detail', arguments: product.id);
-              },
-              child: Image.network(
-                product.imageUrl,
-                fit: BoxFit.cover,
-              )),
-        ),
-        footer: GridTileBar(
-          backgroundColor: Colors.black54,
-          leading: IconButton(
-            icon: Icon(
-              product.isFavorite ? Icons.favorite : Icons.favorite_border,
+    //final product = Provider.of<Product>(context);
+    return Consumer<Product>(
+      builder: (ctx, product, child) => ClipRRect(
+        borderRadius: BorderRadius.circular(5),
+        child: GridTile(
+          child: Container(
+            decoration: BoxDecoration(
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black87,
+                  blurRadius: 10.0,
+                  spreadRadius: 7.0,
+                ),
+              ],
             ),
-            color: Theme.of(context).accentColor,
-            onPressed: () {
-              product.toggleFavoriteStatus();
-            },
+            child: GestureDetector(
+                onTap: () {
+                  Navigator.of(context)
+                      .pushNamed('/product-detail', arguments: product.id);
+                },
+                child: Image.network(
+                  product.imageUrl,
+                  fit: BoxFit.cover,
+                )),
           ),
-          trailing: IconButton(
-            icon: Icon(Icons.shopping_cart),
-            color: Theme.of(context).accentColor,
-            onPressed: () {},
+          footer: GridTileBar(
+            backgroundColor: Colors.black54,
+            leading: IconButton(
+              icon: Icon(
+                product.isFavorite ? Icons.favorite : Icons.favorite_border,
+              ),
+              color: Theme.of(context).accentColor,
+              onPressed: () {
+                product.toggleFavoriteStatus();
+              },
+            ),
+            trailing: IconButton(
+              icon: Icon(Icons.shopping_cart),
+              color: Theme.of(context).accentColor,
+              onPressed: () {},
+            ),
+            title: Text(
+              product.title,
+              textAlign: TextAlign.center,
+            ),
           ),
-          title: Text(
-            product.title,
-            textAlign: TextAlign.center,
-          ),
-        ),
-      ), //GridTile
+        ), //GridTile
+      ),
     );
   }
 }
