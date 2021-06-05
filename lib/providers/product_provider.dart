85e5e7fc9df11eb8749065a6c4a51979244323dd
Beren Erchamion <beren@houseofbeor.net>
2021-06-05 16:52:36 -0400

    setting favorites on the server

--- lib/providers/product_provider.dart
+++ lib/providers/product_provider.dart
@@ -1,12 +1,25 @@
 import 'package:flutter/foundation.dart';
+import 'package:http/http.dart';
+import 'dart:convert';
 
-class Product with ChangeNotifier{
-  @required final String id;
-  @required final String title;
-  @required final String description;
-  @required final double price;
-  @required final String imageUrl;
+import '../models/http_exception.dart';
+import '../models/product_exception.dart';
+
+class Product with ChangeNotifier {
+  @required
+  final String id;
+  @required
+  final String title;
+  @required
+  final String description;
+  @required
+  final double price;
+  @required
+  final String imageUrl;
   bool isFavorite;
+
+  final String _baseUrl =
+      'https://hob-shop-default-rtdb.firebaseio.com/products';
 
   Product({
     this.id,
@@ -17,8 +30,26 @@ class Product with ChangeNotifier{
     this.isFavorite = false,
   });
 
-  toggleFavoriteStatus() {
-    isFavorite = !isFavorite;
+  void _setFavoriteStatus (bool newValue) {
+    isFavorite = newValue;
     notifyListeners();
+  }
+
+  Future<void> toggleFavoriteStatus() async {
+    final url = '$_baseUrl/${this.id}.json';
+    Uri uri = Uri.parse(url);
+    final currentFavoriteStatus = isFavorite;
+    _setFavoriteStatus(!isFavorite);
+    try {
+      final response = await patch(uri,
+          body: json.encode({
+            'isFavorite': isFavorite,
+          }));
+      if (response.statusCode >= 400) {
+        throw (HttpException('HTTP $response.statusCode error while setting product favorite value.'));
+      }
+    } catch (error) {
+      _setFavoriteStatus(currentFavoriteStatus);
+    }
   }
 }
